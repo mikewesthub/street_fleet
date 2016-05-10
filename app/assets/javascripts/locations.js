a5279d057173dffa3d1bc603842dc8db51c23c28
Mike West <westmd23@gmail.com>
2016-05-10 14:40:46 -0400

    can now display locations as points on map

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -1,17 +1,40 @@
 console.log('HI');
-$( document ).ready( function() {
+$(document).ready(function() {
 
-  if ($(".map-container").length > 0 ) {
+  if ($(".map-container").length > 0) {
 
-  mapboxgl.accessToken = 'pk.eyJ1Ijoid2VzdG1kMjMiLCJhIjoiY2lvMHNhZ3gyMTl6aXRxa2pnbDM0ajBzOCJ9.f-skllhdyAEgwGO_UZhk8Q';
+    mapboxgl.accessToken = 'pk.eyJ1Ijoid2VzdG1kMjMiLCJhIjoiY2lvMHNhZ3gyMTl6aXRxa2pnbDM0ajBzOCJ9.f-skllhdyAEgwGO_UZhk8Q';
     var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v8',
-        center: [
-          -77.03716278076172,
-          38.90198809660059
-         ],
-        zoom: 12
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v8',
+      center: [-77.03716278076172,
+        38.90198809660059
+      ],
+      zoom: 11
     });
-  }
+
+    map.on('load', function() {
+        map.addSource("markers", {
+            "type": "geojson",
+            "data": {
+              "type": "FeatureCollection",
+              "features": latlong_array
+          }
+        });
+
+      map.addLayer({
+        "id": "markers",
+        "type": "symbol",
+        "source": "markers",
+        "layout": {
+          "icon-image": "{marker-symbol}-15",
+          "text-field": "{title}",
+          "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+          "text-offset": [0, 0.6],
+          "text-anchor": "top"
+        }
+      });
+    });
+
+}
 });
