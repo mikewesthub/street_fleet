3fb9aa72f4c78c6afad96bb847895adfbc94a944
Mike West <westmd23@gmail.com>
2016-05-12 15:28:33 -0400

    add reverse geocoding to display addresses in view layer

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,6 @@ class Location < ActiveRecord::Base
   belongs_to :truck
   validates :longitude, presence: true
   validates :latitude, presence: true
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :reverse_geocode 
 end
--- app/models/truck.rb
+++ app/models/truck.rb
@@ -2,7 +2,7 @@ class Truck < ActiveRecord::Base
   has_many :locations
   belongs_to :user
   validates :name, presence: true
-
+  
   def most_recent_location
     locations.order(id: :desc).limit(1)
   end
