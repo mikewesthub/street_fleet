6159ba8de61912c6ffcd025882bc694c9c68befb
Mike West <westmd23@gmail.com>
2016-05-17 17:32:24 -0400

    added user controller and view for dashboard creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'twitter/index'
-
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks'}
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
 
+  get 'user/show'
+  get 'twitter/index'
   get 'home/show'
+
   resources :trucks do
     resources :locations, shallow: true
   end
