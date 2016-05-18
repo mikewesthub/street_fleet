23b6beb1523728921b38e63a824f6c04bcc1d642
Mike West <westmd23@gmail.com>
2016-05-17 23:27:56 -0400

    started to set up profile

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe UserController, type: :controller do
+  setup do
+      @user = User.create!({email: "me@gmail.com", password: "123456", password_confirmation: "123456"})
+      sign_in @user
+  end
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      @user = User.create!({email: "me@gmail.com", password: "123456", password_confirmation: "123456"})
+      get :show, { id: @user.to_param }
       expect(response).to have_http_status(:success)
     end
   end
